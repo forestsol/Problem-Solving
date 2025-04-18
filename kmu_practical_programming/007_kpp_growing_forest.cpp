433ae534f86c28cc9a89395ad97cbbd7cca83f9c
forestsol <51287968+forestsol@users.noreply.github.com>
2025-04-18 15:51:32 +0900

    feat: PS 2025-04-18

---
+++ kmu_practical_programming/007_kpp_growing_forest.cpp
@@ -0,0 +1,20 @@
+/***********************************************
+ * 2025-04-17
+ ***********************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+int main() {
+    fastio;
+    int t;
+    cin >> t;
+    while(t-- > 0){
+        
+    }
+
+    return 0;
+}

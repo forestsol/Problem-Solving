9fa63e2a633651ade1b64e22fbfe383c2fd2124e
forestsol <forestsol1214@gmail.com>
2025-08-13 22:20:52 +0900

    feat: boj_15727 solved
    feat: boj_15727 solved

---
+++ boj/026_boj_15727_조별과제를_하려는데_조장이_사라졌다.cpp
@@ -0,0 +1,21 @@
+/***********************************************
+ * 2025-08-13
+ ***********************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+int main() {
+    fastio;
+    double l;
+    cin >> l;
+    double t = 0;
+    t = l/5;
+
+    cout << ceil(t) << endl; 
+    // 알고리즘 문제 풀이 시작
+    return 0;
+}

8f663d71294fb92ac16d52951493df98179094c5
forestsol <51287968+forestsol@users.noreply.github.com>
2025-08-16 00:18:28 +0900

    feat: boj_10814 solved
    feat: boj_10814 solved

--- boj/034_boj_10814_나이순_정렬.cpp
+++ boj/034_boj_10814_나이순_정렬.cpp
@@ -14,9 +14,20 @@ int main() {
     cin >> n;
 
     vector<pair<int, string>> arr;
+    arr.reserve(n);
 
     for(int i = 0 ; i < n ; i++){
-        
+        int age;
+        string name;
+        cin >> age >> name;
+        arr.emplace_back(age,name);
+    }   
+    stable_sort(arr.begin(), arr.end(),[](const pair<int, string>& a, const pair<int, string>& b){
+        return a.first < b.first;
+    });
+
+    for(auto &p : arr){ 
+        cout << p.first << " " << p.second << endl;
     }
     // 알고리즘 문제 풀이 시작
     return 0;

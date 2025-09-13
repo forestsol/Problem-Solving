2a48f322e9934ed3e1ec5216a6ade8bfb8a44c75
forestsol <forestsol1214@gmail.com>
2025-09-13 23:21:54 +0900

    solved: boj_ 1929, 17103
    solved: boj_ 1929, 17103

--- boj_latest/072_boj_17103.cpp
+++ boj_latest/072_boj_17103.cpp
@@ -1,5 +1,5 @@
 /***********************************************
- * 2025-09-12
+ * 2025-09-13
  ***********************************************/
 
 #include <bits/stdc++.h>
@@ -8,12 +8,12 @@ using namespace std;
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define endl '\n'
 
-vector<bool> sieve(int m, int n){  // 에라토스테네스의 채.
+vector<bool> sieve(int n){
     vector<bool> is_prime(n+1, true);
     is_prime[0] = is_prime[1] = false;
-    for(int i = 2; i*i <= n; ++i){
+    for(int i = 2; i*i < n; ++i){
         if(is_prime[i]){
-            for(int j = i*i; j <= n; j += i){
+            for(int j = i*i ; j <= n ; j += i){
                 is_prime[j] = false;
             }
         }
@@ -23,13 +23,22 @@ vector<bool> sieve(int m, int n){  // 에라토스테네스의 채.
 
 int main() {
     fastio;
-    int start, end;
-    cin >> start >> end;
-    auto primes = sieve(start, end);
-    for(int i = start; i <= end; i++ ){
-        if(primes[i]){
-            cout << i << endl;
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; ++i){
+        int n;
+        int cnt = 0;
+        cin >> n;
+        vector<bool> primes = sieve(n);
+        for(int j = 2; j <= n/2; ++j){
+            bool temp1 = primes[j];
+            bool temp2 = primes[n-j];
+            if(primes[j] && primes[n-j]){
+                cnt++;
+            }
         }
+        cout << cnt << endl;
     }
     // 알고리즘 문제 풀이 시작
     return 0;

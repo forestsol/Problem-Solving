00b23d510547ff48550b3fcd9e33744468303032
forestsol <51287968+forestsol@users.noreply.github.com>
2025-09-28 22:36:36 +0900

    Update 087_boj_20207.cpp

--- boj_latest/087_boj_20207.cpp
+++ boj_latest/087_boj_20207.cpp
@@ -10,7 +10,32 @@ using namespace std;
 
 int main() {
     fastio;
+    int n;
+    cin >> n;
+    vector<int> calander(367 ,0);
+    for(int i = 0; i<n; i++){
+        int s, e;
+        cin >> s >> e;
+        for(int d = s; d <= e; d++){
+            calander[d]++;
+        }
+    }
+    int width = 0, height = 0, answer = 0;
+    for(int d = 1; d <= 366 ; d++){
+        if(calander[d] > 0){
+            width++;
+            height = max(calander[d], height);
+        }else{
+            if(width > 0){
+                answer += width * height;
+                width = 0;
+                height = 0;
+            }
+        }
+    }
+    if (width > 0) answer += width * height;
 
+    cout << answer << endl;
     // 알고리즘 문제 풀이 시작
     return 0;
 }

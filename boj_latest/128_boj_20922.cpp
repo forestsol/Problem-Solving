150829761203854566bba17735bd2407a97f1aba
forestsol <51287968+forestsol@users.noreply.github.com>
2025-12-02 01:12:36 +0900

    Update 128_boj_20922.cpp

--- boj_latest/128_boj_20922.cpp
+++ boj_latest/128_boj_20922.cpp
@@ -9,9 +9,41 @@ typedef long long ll;
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define endl '\n'
 
+int n, k;
+vector<int> a;
+vector<int> cnt(100001, 0);
+
+void input(){
+    cin >> n >> k;
+    a.resize(n);
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+}
+
+void solution(){
+    input();
+
+    int left = 0;
+    int answer = 0;
+
+    for(int right = 0; right < n; ++right){
+        int x = a[right];
+        cnt[x]++;
+
+        while(cnt[x] > k){
+            cnt[a[left]]--;
+            left++;
+        }
+
+        answer = max(answer, right-left + 1);
+    }
+    cout << answer << endl;
+}
+
 int main() {
     fastio;
-
+    solution();
     // 알고리즘 문제 풀이 시작
     return 0;
 }

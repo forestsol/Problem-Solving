fd8e0682e1f79075b31b25ac723f40348155f4ee
Changmin Sung <51287968+forestsol@users.noreply.github.com>
2026-06-15 14:12:05 +0900

    edit readme

---
+++ inflearn/inflearntest.cpp
@@ -0,0 +1,19 @@
+/***********************************************
+ * 2026-06-15
+ ***********************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+int main() {
+    fastio;
+    // 알고리즘 문제 풀이 시작
+    cout << "hello" << endl;
+    int x;
+    cin >> x;
+    cout << x;
+    return 0;
+}
